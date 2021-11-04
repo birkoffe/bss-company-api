76842869dca789aec3fb3aae147af1eee79200fd
birkoffe <birkoff.anarchist@gmail.com>
2021-11-04 22:43:38 +0700

    fix typo in py-file

--- scripts/grpc_client.py
+++ scripts/grpc_client.py
@@ -9,7 +9,7 @@ async def main():
     async with Channel('127.0.0.1', 8082) as channel:
         client = BssCompanyApiServiceStub(channel)
 
-        req = DescribecompanyV1Request(company_id=1)
+        req = DescribeCompanyV1Request(company_id=1)
         reply = await client.DescribecompanyV1(req)
         print(reply.message)
 
